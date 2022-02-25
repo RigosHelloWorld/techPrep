123425cb8aeec1d2d3f754fbeb211639247ec5ab
Rigoberto Rosa <rigorosa@bu.edu>
2022-02-25 16:27:30 -0500

    third commit

---
+++ techPrep/leetcode/JavaSolutions/Leetcode1920.java
@@ -0,0 +1,16 @@
+import HelperClasses.MyArrays;
+
+
+
+class Leetcode1920 {
+    
+    public static void main(String[] args) {
+
+  
+     MyArrays myArrays = new MyArrays();
+
+
+       System.out.println(myArrays.toString());
+    }
+
+}
