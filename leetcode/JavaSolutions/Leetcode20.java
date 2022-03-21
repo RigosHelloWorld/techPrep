a5c785b6f9fe5719e99edd83352415d3487b27f7
Rigoberto Rosa <rigorosa@bu.edu>
2022-03-21 11:39:36 -0400

    added solution for problem 20 on leetcode

---
+++ leetcode/JavaSolutions/Leetcode20.java
@@ -0,0 +1,66 @@
+package leetcode.javaSolutions;
+
+import java.util.Stack;
+
+class Leetcode20 {
+    public boolean isValid(String s) {
+        
+         if((s.length() % 2) != 0 ) return false;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i < s.length(); i++){
+            
+            Character c = s.charAt(i);
+            
+            switch(c){
+                    case(')'):
+                    case(']'):
+                    case('}'):{
+                        if(!stack.isEmpty()){
+                            if(!stackHelper(stack,c)) return false;
+                        }
+                        else return false;
+                        break;
+                    }
+                default:{
+                    stack.add(c);
+                }
+            }
+            
+            
+            
+            
+        }//end of for loop
+        
+        return stack.isEmpty();
+    }
+    
+    private boolean stackHelper(Stack stack, Character c ){
+        
+        switch(c){
+                case(')'):{
+                    if(stack.peek().equals('(') ){
+                        stack.pop();
+                        return true;
+                    }
+                    break;
+                    
+                }
+                case(']'):{
+                    if(stack.peek().equals('[') ){
+                        stack.pop();
+                        return true;
+                    }
+                 break;
+                }case('}'):{
+                    if(stack.peek().equals('{') ){
+                        stack.pop();
+                        return true;
+                    }
+                   break;
+                }
+        }
+        return false;
+    }
+}
